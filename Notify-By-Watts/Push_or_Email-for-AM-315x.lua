b0d8a9006cbcfa8a867f6cf202b98c5c68ec0a77
snille <mrsnille@gmail.com>
2018-06-08 14:25:29 +0200

    Smaller changes.

--- Notify-By-Watts/Push_or_Email-for-AM-315x.lua
+++ Notify-By-Watts/Push_or_Email-for-AM-315x.lua
@@ -6,20 +6,21 @@
 --]] 
 
 local id = 199 -- AM Wallplugg ID.
-local donevalue = 8 -- When device uses less watt then this then it's done.
-local timer1 = 600 -- How often to check watt usage in seconds (600 = 10 minutes) when in the "wait loop".
-local timer2 = 180 -- How often to check watt usage in seconds (180 = 3 minutes) when in the "rechargeing loop".
-local counter = 3 -- Counter used for counting wait timer2 loops before sending done message, to make sure it's really done (timer2 * counter = time to wait).
-local touser = 230 -- The user to receive the messages (230 = Usernumber).
-local thing = "AM-315X" -- The Automowers name we are monitoring (for the debugging text output).
-local messagenr1 = "261" -- Message number from notification list when rechargeing is finnished.
-local messagenr2 = "259" -- Message number from notification list when rechargeing begins.
+local donevalue = 10 -- When device uses less watt then this then it's done.
+local timer1 = 300 -- How often to check watt usage in seconds (600 = 10 minutes) when in the "wait loop".
+local timer2 = 60 -- How often to check watt usage in seconds (60 = 1 minute) when in the "rechargeing loop".
+local counter = 5 -- Counter used for counting wait timer2 loops before sending done message, to make sure it's really done (timer2 * counter = time to wait).
+local touser = 230 -- The user to receive the messages (230 = Snilles OnePlus3).
+local thing = "Rupert" -- The Automowers name we are monitoring (for the debugging text output).
+local messagenr1 = "259" -- Message number from notification list when rechargeing is finnished.
+local messagenr2 = "261" -- Message number from notification list when rechargeing begins.
 local notifytype = "push" -- Can be set to either "email" or "push".
 
 -- Don't change below!!
 
 local messsent = 1 -- Sets to 1/0 if a message is sent / reset.
 local count = 0 -- Sets to same as counter when done wating for the message to be sent.
+local start = 0 -- Sets to 1 when the loadcycle has started and first message has been sent.
 local sourceTrigger = fibaro:getSourceTrigger(); -- Check if autostart.
 
 -- Check power consumtion if plug is on.
@@ -48,15 +49,6 @@ function mainloop()
   if (messsent == 0 and status == 1) then
     if (watts < donevalue) then
       count = count+1
-      if (count == 1) then
-        if (notifytype == "email") then
-          fibaro:call(touser, "sendEmail", messagenr2);
-        elseif (notifytype == "push") then
-          fibaro:call(touser, "sendDefinedPushNotification", messagenr2);
-        else
-          fibaro:debug (thing.. " at docking station and starts rechargeing! This debug message sent (nor email or push was selected).\nState is: " ..status.. " and watt is: " ..watts);
-        end
-      end
       if (count > counter) then
         if (notifytype == "email") then
           fibaro:call(touser, "sendEmail", messagenr1);
@@ -68,12 +60,24 @@ function mainloop()
         fibaro:debug (thing.. " is done rechargeing! Messages sent.\nState is: " ..status.. " and watt is: " ..watts);
         count = 0
         messsent = 1
+        start = 0
       else 
         fibaro:debug (thing.. " is probably done rechargeing. No messages sent yet, wait " ..count.. "/" ..counter.. " to be sure.\nState is: " ..status.. " and watt is: " ..watts);
         fibaro:sleep(timer2*1000)
         mainloop();
       end
     else
+      if (start == 0) then
+        if (notifytype == "email") then
+          fibaro:call(touser, "sendEmail", messagenr2);
+        elseif (notifytype == "push") then
+          fibaro:call(touser, "sendDefinedPushNotification", messagenr2);
+        else
+          fibaro:debug (thing.. " at docking station and starts rechargeing! This debug message sent (nor email or push was selected).\nState is: " ..status.. " and watt is: " ..watts);
+        end
+        fibaro:debug (thing.. " at docking station and starts rechargeing! \nState is: " ..status.. " and watt is: " ..watts);
+        start = 1
+      end
       fibaro:debug (thing.. " is still rechargeing!\nState is: " ..status.. " and watt is: " ..watts);
       messsent = 0
       count = 0
