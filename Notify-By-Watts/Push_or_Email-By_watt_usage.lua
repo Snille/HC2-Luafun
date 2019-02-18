44e6bb99276dd2926f2800e7e092fea61e93b792
Erik Pettersson <master_snille@hotmail.com>
2019-02-18 22:58:29 +0100

    Debug messages on/off
    Added support for debug messages on/off using the global variable "globaldebug".

--- Notify-By-Watts/Push_or_Email-By_watt_usage.lua
+++ Notify-By-Watts/Push_or_Email-By_watt_usage.lua
@@ -6,14 +6,16 @@
 --]] 
 
 -- Dont forget to change the number before "valueSensor" above to the same number as ID below!!
-local id = 156 -- Thing Wallplugg ID we are monitoring.
+local id = 156 -- Thing Wallplugg ID. 
 local donevalue = 5 -- When the appliance uses less watt then this then it's done.
 local timer = 300 -- How often to check watt usage in seconds (300 = 5 minutes).
 local counter = 4 -- Counter used for counting wait timer loops before sending done message, to make sure the dishwasher is really done (timer * counter = time to wait).
-local touser = fibaro:getGlobalValue("msguser01"); -- The user to receive the done-message (You must create the global variable for the user who should receive the message in the “Variables Panel”).
+local touser = fibaro:getGlobalValue("msguser01"); -- The user to receive the done-message (I make global variable for this).
 local thing = "Dishwasher" -- The thing we are monitoring (for the debugging text output).
-local messagenr = "122" -- Message number from notification list (you must create the message in the “Notifications Panel”, you can see the message number in the URL).
+local messagenr = "122" -- Message number from notification list.
 local notifytype = "push" -- Can be set to either "email" or "push".
+local debug = tonumber(fibaro:getGlobalValue("globaldebug")); -- Set to one to activate debugging (show all messages).
+--local debug = 1 -- Set to one to activate debugging (show all messages).
 
 -- Don't change below!!
 
@@ -53,25 +55,25 @@ function mainloop()
         elseif (notifytype == "push") then
           fibaro:call(touser, "sendDefinedPushNotification", messagenr);
         else
-          fibaro:debug (thing.. " is done! This debug message sent (nor email or push was selected).\nState is: " ..status.. " and watt is: " ..watts);
+          if debug > 0 then fibaro:debug (thing.. " is done! This debug message sent (nor email or push was selected).\nState is: " ..status.. " and watt is: " ..watts); end
         end
-        fibaro:debug (thing.. " is done! Messages sent.\nState is: " ..status.. " and watt is: " ..watts);
+        if debug > 0 then fibaro:debug (thing.. " is done! Messages sent.\nState is: " ..status.. " and watt is: " ..watts); end
         count = 0
         messsent = 1
       else 
-        fibaro:debug (thing.. " is probably done. No messages sent yet, wait " ..count.. "/" ..counter.. " to be sure.\nState is: " ..status.. " and watt is: " ..watts);
+        if debug > 0 then fibaro:debug (thing.. " is probably done. No messages sent yet, wait " ..count.. "/" ..counter.. " to be sure.\nState is: " ..status.. " and watt is: " ..watts); end
         fibaro:sleep(timer*1000)
 		mainloop();
 	  end
     else
-      fibaro:debug (thing.. " is running!\nState is: " ..status.. " and watt is: " ..watts);
+      if debug > 0 then fibaro:debug (thing.. " is running!\nState is: " ..status.. " and watt is: " ..watts); end
 	  messsent = 0
 	  count = 0
       fibaro:sleep(timer*1000)
       mainloop();
 	end
   end
-  fibaro:debug ("Wating for " ..thing.. " to start.\nState is: " ..status.. " and watt is: " ..watts);
+  if debug > 0 then fibaro:debug ("Wating for " ..thing.. " to start.\nState is: " ..status.. " and watt is: " ..watts); end
   if (messsent == 1 and watts > donevalue) then
     messsent = 0
   end
@@ -81,10 +83,10 @@ end
 
 status = checkstatus();
 watts = checkwatts();
-fibaro:debug ("Start - " ..thing.. " plug is in state "..status.. " and consums " ..watts.. " watt.");
+if debug > 0 then fibaro:debug ("Start - " ..thing.. " plug is in state "..status.. " and consums " ..watts.. " watt."); end
 
 if (sourceTrigger["type"] == "autostart") then
 	mainresult = mainloop();
 else
-  fibaro:debug ("No auto start...");
+  if debug > 0 then fibaro:debug ("No auto start..."); end
 end
