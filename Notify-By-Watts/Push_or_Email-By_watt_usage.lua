c965751b89540c1578c04dac7d36ef64d9ae71af
Erik Pettersson <master_snille@hotmail.com>
2019-02-20 23:55:54 +0100

    Added options for Sonos messages.
    Added options for playing messages on a Sonos player between specified times. Fixed spelling errors...

--- Notify-By-Watts/Push_or_Email-By_watt_usage.lua
+++ Notify-By-Watts/Push_or_Email-By_watt_usage.lua
@@ -2,28 +2,34 @@
 %% autostart
 %% properties 
 156 valueSensor
+%% weather
+%% events
 %% globals 
 --]] 
 
--- Dont forget to change the number before "valueSensor" above to the same number as ID below!!
-local id = 156 -- Thing Wallplugg ID. 
+-- Dont forget to change the number before "valueSensor" above to the same number as Wall plug ID below!!
+local id = 156 -- Thing Wall plug ID. 
 local donevalue = 5 -- When the appliance uses less watt then this then it's done.
 local timer = 300 -- How often to check watt usage in seconds (300 = 5 minutes).
-local counter = 4 -- Counter used for counting wait timer loops before sending done message, to make sure the dishwasher is really done (timer * counter = time to wait).
-local touser = fibaro:getGlobalValue("msguser01"); -- The user to receive the done-message (I make global variable for this).
+local counter = 4 -- Counter used for counting wait timer loops before sending done message, to make sure the appliance is really done (timer * counter = time to wait).
+local touser = fibaro:getGlobalValue("msguser01"); -- The user to send the done-message to (I make global variable called "msguser01" for this).
 local thing = "Dishwasher" -- The thing we are monitoring (for the debugging text output).
 local messagenr = "122" -- Message number from notification list.
-local notifytype = "push" -- Can be set to either "email" or "push".
-local debug = tonumber(fibaro:getGlobalValue("globaldebug")); -- Set to one to activate debugging (show all messages).
---local debug = 1 -- Set to one to activate debugging (show all messages).
+local notifytype = "push" -- How the message should be delivered. Either "email" or "push" (default push).
+local sonossay = 1 -- Set to 1 if you want a message read on the Sonos player (using a Virtual Device for this action).
+local sonosvirDevID = 294  -- Sonos Virtual Action Device ID.
+local sonosactionid = 2 -- The "button-ID" on the Sonos Virtual Action Device to be triggered (basically the defined action sound to play in the VDevice).
+local latest = tonumber(fibaro:getGlobalValue("timelatest")); -- Set a global variable called "timelatest" with the latest hour (24h time) a sound is allowed to be played.
+local earliest = tonumber(fibaro:getGlobalValue("timeearliest")); -- Set a global variable called "timeearliest" to the earliest hour (24h time) a sound is allowed to be played.
+local debug = tonumber(fibaro:getGlobalValue("globaldebug")); -- Set the global variable called "globaldebug" to one to activate debugging (show all messages).
+--local debug = 1 -- Set to one to activate debugging (show all messages). Enable this line to override the global debug variable.
 
--- Don't change below!!
-
+-- Don't change anything below this line!!
 local messsent = 1 -- Sets to 1/0 if a message is sent / reset.
-local count = 0 -- Sets to same as counter when done wating for the message to be sent.
+local count = 0 -- Sets to same as counter when done waiting for the message to be sent.
 local sourceTrigger = fibaro:getSourceTrigger(); -- Check if autostart.
 
--- Check power consumtion if plug is on.
+-- Check power consumption if plug is on.
 function checkwatts()
   if (tonumber(fibaro:getValue(id, "value")) == 1) then
     wallplugwatt = tonumber(fibaro:getValue(id, "power"));
@@ -51,11 +57,16 @@ function mainloop()
       count = count+1
       if (count > counter) then
 		if (notifytype == "email") then
-          fibaro:call(touser, "sendEmail", messagenr);
+          fibaro:call(touser, "sendDefinedEmailNotification", messagenr);
         elseif (notifytype == "push") then
           fibaro:call(touser, "sendDefinedPushNotification", messagenr);
         else
           if debug > 0 then fibaro:debug (thing.. " is done! This debug message sent (nor email or push was selected).\nState is: " ..status.. " and watt is: " ..watts); end
+        end
+        if (sonossay > 0) then
+          if ((tonumber(currenthour) <= latest ) and ( tonumber(currenthour) >= earliest )) then
+            fibaro:call(sonosvirDevID, "pressButton", sonosactionid);
+          end
         end
         if debug > 0 then fibaro:debug (thing.. " is done! Messages sent.\nState is: " ..status.. " and watt is: " ..watts); end
         count = 0
@@ -73,7 +84,7 @@ function mainloop()
       mainloop();
 	end
   end
-  if debug > 0 then fibaro:debug ("Wating for " ..thing.. " to start.\nState is: " ..status.. " and watt is: " ..watts); end
+  if debug > 0 then fibaro:debug ("Waiting for " ..thing.. " to start.\nState is: " ..status.. " and watt is: " ..watts); end
   if (messsent == 1 and watts > donevalue) then
     messsent = 0
   end
@@ -83,8 +94,9 @@ end
 
 status = checkstatus();
 watts = checkwatts();
-if debug > 0 then fibaro:debug ("Start - " ..thing.. " plug is in state "..status.. " and consums " ..watts.. " watt."); end
+if debug > 0 then fibaro:debug ("Start - " ..thing.. " plug is in state "..status.. " and consumes " ..watts.. " watt."); end
 
+-- Starting the main loop.
 if (sourceTrigger["type"] == "autostart") then
 	mainresult = mainloop();
 else
